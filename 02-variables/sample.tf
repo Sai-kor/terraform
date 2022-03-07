d7e685bedc6e4b26d66ed3bafb5c0ad99973cdfb
sai k <psai0073@gmail.com>
2022-03-07 18:30:16 -0500

    tf vars sample empty variable block

--- 02-variables/sample.tf
+++ 02-variables/sample.tf
@@ -5,3 +5,7 @@ variable "demo" {
 output "demo" {
   value = var.demo //reference of variable from a resource
 }
+
+// can declare an empty variable too ,then tf interrupts and asks for user input
+
+variable "demo1" {}

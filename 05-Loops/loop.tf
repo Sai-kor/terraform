b99ae92a11fcdb7a890b81a6451816911d645ee7
sai k <psai0073@gmail.com>
2022-03-08 16:04:25 -0500

    loops demo count

--- 05-Loops/loop.tf
+++ 05-Loops/loop.tf
@@ -1,8 +1,9 @@
 resource "random_string" "random" {
+  count            = 3
   length           = 16
   special          = false
 }
 
-output "random_string" {
-  value = random_string.random.upper
-}
+#output "random_string" {
+#  value = random_string.random.upper
+#}
